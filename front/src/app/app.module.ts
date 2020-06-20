59044fe3a9c8fe41a99a56c9fae1acbaacb96a96
David Rueda <hunternewlife@hotmail.com>
2020-06-20 11:12:47 -0500

    Creando componentes basicos

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CinemaComponent } from './components/cinema/cinema.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CinemaComponent,
+    LoginComponent,
+    RegistroComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule
