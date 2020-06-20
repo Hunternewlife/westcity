759183b72f90313474b93626671f467d758ceded
roguerre <iv.gubri@yahoo.com>
2020-06-20 12:39:19 -0500

    incluir logica de levantamiento de servidor y rutas en `app.js`

---
+++ back/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+
+// Rutas de la aplicacion
+const usuarioRutas = require('./rutas/usuarioRutas');
+
+// Fin de rutas
+
+// Middlewares
+
+// Para que se pueda reconocer el cuerpo de la peticion como JSON
+app.use(express.json());
+
+// CORS
+app.use(cors());
+
+// Middleware para rutas de usuario
+app.use('/api', usuarioRutas);
+
+// Fin de middlewares
+
+module.exports = app;
