aad7561707f6cbde0bb16e1e5d66e7ba9b532284
roguerre <iv.gubri@yahoo.com>
2020-06-20 13:34:00 -0500

    Incluir funcion generica para registrar nuevos usuarios

---
+++ back/control/usuarioControl.js
@@ -0,0 +1,24 @@
+// Modelo de usuario (para operaciones a traves de mongoose)
+const Usuario = require("../modelo/usuario");
+
+const fs = require("fs");
+const path = require("path");
+
+// Funcion generica para crear un nuevo documento de usuario
+function crearUsuario(req, res) {
+  // Utilizar `object destructuring` para mantener el codigo conciso
+  const usuario = new Usuario({ ...req.body });
+
+  usuario.save((err, usuarioNuevo) => {
+    if (err) {
+      res.status(500).send({ message: "Error en el servidor" });
+    } else if (!usuarioNuevo) {
+      res.status(200).send({ message: "No fue posible registrar el usuario" });
+    } else {
+      res.status(200).send({
+        message: "Usuario creado",
+        usuario: usuarioNuevo,
+      });
+    }
+  });
+}
