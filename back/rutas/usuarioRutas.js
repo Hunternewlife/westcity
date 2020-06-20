deda2f6a4b707faf557a1aab2eff0e483acbdaf3
roguerre <iv.gubri@yahoo.com>
2020-06-20 14:37:16 -0500

    Incluir ruta para login de usuario

--- back/rutas/usuarioRutas.js
+++ back/rutas/usuarioRutas.js
@@ -1,10 +1,13 @@
-const express = require('express');
-const UsuarioControl = require('../control/usuarioControl');
+const express = require("express");
+const UsuarioControl = require("../control/usuarioControl");
 
 var api = express.Router();
 
 // Ruta para registrar usuario
-api.post('/registro', UsuarioControl.registrarUsuario);
+api.post("/registro", UsuarioControl.registrarUsuario);
+
+// Ruta para login de usuario
+api.post("/login", UsuarioControl.loginUsuario);
 
 // Exportar el modulo
 module.exports = api;
