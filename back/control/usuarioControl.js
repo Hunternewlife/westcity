d1d2f70554023e468289fab6ff51f190a47011e3
roguerre <iv.gubri@yahoo.com>
2020-06-20 14:24:38 -0500

    Corregir logica de validacion en registro

--- back/control/usuarioControl.js
+++ back/control/usuarioControl.js
@@ -9,17 +9,35 @@ function registrarUsuario(req, res) {
   // Utilizar `object destructuring` para mantener el codigo conciso
   const usuario = new Usuario({ ...req.body });
 
-  usuario.save((err, usuarioNuevo) => {
-    if (err) {
-      res.status(500).send({ message: "Error al registrar usuario" });
-    } else if (!usuarioNuevo) {
-      res.status(200).send({ message: "No fue posible registrar el usuario" });
-    } else {
+  // Es importante que el correo se encuentre normalizado para facilitar
+  // validacion del mismo
+  usuario.correo = usuario.correo && usuario.correo.toLowerCase();
+
+  // Antes de almacenar verificar que el correo no se encuentra registrado
+  Usuario.findOne({ correo: usuario.correo }, (err, usuarioExistente) => {
+    if (err) res.status(500).send({ message: "Error al registrar usuario" });
+    else if (usuarioExistente)
       res.status(200).send({
-        message: "Usuario registrado",
-        usuario: usuarioNuevo,
+        message: "No fue posible registrar el usuario, correo existente",
       });
-    }
+    // Aun pueden fallar las otras validaciones
+    else
+      usuario.save((err, usuarioNuevo) => {
+        if (err) {
+          res.status(500).send({
+            message: "Error al registrar usuario,  validaciones no exitosas",
+          });
+        } else if (!usuarioNuevo) {
+          res.status(200).send({
+            message: "No fue posible registrar el usuario",
+          });
+        } else {
+          res.status(200).send({
+            message: "Usuario registrado",
+            usuario: usuarioNuevo,
+          });
+        }
+      });
   });
 }
 
