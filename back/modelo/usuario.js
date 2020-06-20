616510eb05f3584ecfc87d1f2ce0b53f1e68e9ba
roguerre <iv.gubri@yahoo.com>
2020-06-20 13:28:26 -0500

    Modificar modelo de usuario en el back incluyendo validaciones

--- back/modelo/usuario.js
+++ back/modelo/usuario.js
@@ -2,16 +2,52 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-// por ahora, ningun campo es requerido (para facilitar tests)
+// Se incluye validacion (campos requeridos y valores por defecto)
 const UsuarioSchema = new Schema({
-  nombre: String,
-  apellido: String,
-  documento: Number,
-  contrasena: String,
-  correo: String,
-  tarjeta: Number,
-  suscripcion: String,
-  estado: String,
-  imagen: String,
-  rol: String,
+  nombre: {
+    type: String,
+    required: true,
+  },
+  apellido: {
+    type: String,
+    required: true,
+  },
+  documento: {
+    type: Number,
+    required: true,
+  },
+  contrasena: {
+    type: String,
+    required: true,
+  },
+  correo: {
+    type: String,
+    required: true,
+  },
+  tarjeta: {
+    type: Number,
+    required: true,
+  },
+  suscripcion: {
+    type: String,
+    required: true,
+    enum: ["sheriff", "pistolero"];
+  },
+  estado: {
+    type: String,
+    default: "activo",
+    enum: ["activo", "inactivo"]
+  },
+  imagen: {
+    type: String,
+    default: "",
+  },
+  rol: {
+    type: String,
+    default: "usuario",
+    enum: ["usuario", "admin"]
+  },
 });
+
+// Exportar el modelo
+module.exports = mongoose.model("Usuario", UsuarioSchema);
