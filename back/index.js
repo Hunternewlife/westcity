9d30445a350404d5a4d566d8374ea154cb57b063
roguerre <iv.gubri@yahoo.com>
2020-06-20 13:14:04 -0500

    Incluir opcion para evitar warning de conexion a mongo

--- back/index.js
+++ back/index.js
@@ -1,17 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const app = require('./app');
+const app = require("./app");
 
 const port = 3000;
 
-mongoose.connect('mongodb://localhost:27017/westcity', (err, res) => {
-  if (err) {
-    console.log(`Error de conexion a base de datos ${err}!`);
-  } else {
-    console.log('Conexion a base de datos realizada!');
+mongoose.connect(
+  "mongodb://localhost:27017/westcity",
+  { useNewUrlParser: true },
+  (err, res) => {
+    if (err) {
+      console.log(`Error de conexion a base de datos ${err}!`);
+    } else {
+      console.log("Conexion a base de datos realizada!");
 
-    app.listen(port, () => {
-      console.log(`Servidor escuchando en el puerto ${port}!`);
-    })
+      app.listen(port, () => {
+        console.log(`Servidor escuchando en el puerto ${port}!`);
+      });
+    }
   }
-});
+);
