510a0c50807e3d10ce9ebec70578414931e2e8cc
roguerre <iv.gubri@yahoo.com>
2020-06-20 18:36:36 -0500

    Utilizar midddleware para subir archivos e incluir ruta

--- back/rutas/usuarioRutas.js
+++ back/rutas/usuarioRutas.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const UsuarioControl = require("../control/usuarioControl");
 
-var api = express.Router();
+// Modulos requeridos para la funcion de subir o mostrar imagen
+const path = require("path");
+const fs = require("fs");
+const fileUpload = require("express-fileupload");
+
+const api = express.Router();
+
+// Utilizar middleware para subir archivos
+api.use(fileUpload());
 
 // Ruta para registrar usuario
 api.post("/registro", UsuarioControl.registrarUsuario);
@@ -11,6 +19,9 @@ api.post("/login", UsuarioControl.loginUsuario);
 
 // Ruta para actualizar usuario
 api.put("/actualizar/:id", UsuarioControl.actualizarUsuario);
+
+// Ruta para subir imagen de usuario
+api.put("/subir-img/:id", UsuarioControl.subirImg);
 
 // Exportar el modulo
 module.exports = api;
