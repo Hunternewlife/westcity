340ee6a52c9b9bc6876c0bdcca33f360cc0d3e45
David Rueda <hunternewlife@hotmail.com>
2020-06-21 12:29:14 -0500

    Fixed some navbar mistakes

--- front/src/app/components/header/header.component.ts
+++ front/src/app/components/header/header.component.ts
@@ -25,14 +25,13 @@ export class HeaderComponent implements OnInit {
       nav.classList.toggle('navactive');  
       // burger animation
       burger.classList.toggle('toggle');
+      
     });
     //animate links
     navlinks.forEach((link, index) =>{
       link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
       console.log(index/7);
     });
-    
-    
   }
 }
 
