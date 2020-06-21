42bd4a9471a00f6232bb3fde04113072afffe267
DanielSanchez98 <ds980220@gmail.com>
2020-06-20 19:22:50 -0500

    estrucutura funcion para generar notificacion

--- back/control/notificacionControl.js
+++ back/control/notificacionControl.js
@@ -1,7 +1,20 @@
-const notificacion = require('../modelo/notificacionCorreo');
+const Notificacion = require('../modelo/notificacionCorreo');
+const nodemailer = require("nodemailer");
 
 function generarNotificacion(req,res){
-    console.log('generar not');
+
+    let notificacion = new Notificacion();
+
+    notificacion.titulo = req.body.titulo;
+    notificacion.tipo = req.body.tipo;
+    notificacion.contenido = req.body.contenido;
+    // logica para realizar el mensaje que se enviará por correo
+
+
+    //logica para enviar por correo el mensaje realizado 
+
+    console.log('not', notificacion);
+    console.log('not cont', notificacion.contenido);
 }
 
 
