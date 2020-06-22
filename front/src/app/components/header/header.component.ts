e0780830f5666f9269861e946f9ed63b76a1cecf
David Rueda <hunternewlife@hotmail.com>
2020-06-22 09:22:44 -0500

    error en el header comentado

--- front/src/app/components/header/header.component.ts
+++ front/src/app/components/header/header.component.ts
@@ -28,10 +28,10 @@ export class HeaderComponent implements OnInit {
       
     });
     //animate links
-     navlinks.forEach((link, index) =>{
-      link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
-      console.log(index/7);
-    });
+     /* navlinks.forEach((link, index) =>{
+       link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
+      console.log(index/7); 
+    });  */
   }
 }
 
