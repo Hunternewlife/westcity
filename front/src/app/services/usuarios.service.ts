c3f5cddebb87d45155350da78c750dad5262d2b1
Jonathan López <jonathan_lopez15@outlook.com>
2020-06-23 11:53:33 -0500

    funcionalidad de login con redireccion a perfil y guardado de informacion del usiario en localstorage

--- front/src/app/services/usuarios.service.ts
+++ front/src/app/services/usuarios.service.ts
@@ -11,6 +11,8 @@ export class UsuariosService {
 
   url = 'http://localhost:3000/api/usuario/';
 
+  public identidad
+
   constructor(
     private _http: HttpClient
   ) { }
@@ -28,6 +30,44 @@ export class UsuariosService {
       options
     ).pipe(map(res => res));
   };
+  logIn(usuarioLogueado){
+    let params = JSON.stringify(usuarioLogueado);
+    let options = {
+      headers: new HttpHeaders( { 'content-type' : 'application/json' } )
+      }
+    
+    return this._http.post(
+      this.url + 'login',
+      params,
+      options
+    ).pipe(map(res => res));
+  }
 
+  obtenerUsuario(){
+    let usuarioAutorizado = JSON.parse(localStorage.getItem('sesion'));
+
+    if (usuarioAutorizado != 'undefined') {
+      this.identidad = usuarioAutorizado;
+    }else{
+      this.identidad = null;
+    }
+    return this.identidad;
+  }
+
+  isLogged(){
+    return JSON.parse(localStorage.getItem('logged')).logged;
+}
+
+  cerrarSesion(){
+    localStorage.removeItem('sesion');
+    localStorage.setItem('logged',JSON.stringify({logged:false}));
+  }
+
+  getRol(){
+    
+    return JSON.parse(localStorage.getItem('sesion')).rol;
+  }
 
 }
+
+
