6ed3fa66c9b743fd6ab91ffbfe24884951d4d5d9
roguerre <iv.gubri@yahoo.com>
2020-06-26 09:51:47 -0500

    Incluir logica para obtencion de peliculas en el servicio

--- front/src/app/services/peliculas.service.ts
+++ front/src/app/services/peliculas.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class PeliculasService {
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-  constructor() { }
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class PeliculasService {
+  // url de la api
+  url: string = 'http://localhost:3000/api/';
+
+  // Inyectar cliente http
+  constructor(private _http: HttpClient) {}
+
+  // Servicio para obtener todas las peliculas
+  obtenerPeliculas() {
+    return this._http
+      .get(this.url + 'obtener-peliculas')
+      .pipe(map((res) => res));
+  }
 }
