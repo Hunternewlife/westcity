3becbcdbb208e6bb8ae5b81bf6a734a04c6f8837
David Rueda <hunternewlife@hotmail.com>
2020-06-26 13:52:41 -0500

    Creado en su totalidad la vista de admin-usuarios

--- front/src/app/components/admin-usuarios/admin-usuarios.component.ts
+++ front/src/app/components/admin-usuarios/admin-usuarios.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminUsuariosComponent implements OnInit {
 
+  filterUsuarios = '';
+  usuarios;
+
   constructor() { }
 
   ngOnInit(): void {
