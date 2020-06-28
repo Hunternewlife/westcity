bfd4d036769ca33080039d41773b3b508fd261ae
roguerre <iv.gubri@yahoo.com>
2020-06-28 13:12:17 -0500

    Incluir metodo auxiliar para poblar el array de usuarios

--- front/src/app/components/admin-usuarios/admin-usuarios.component.ts
+++ front/src/app/components/admin-usuarios/admin-usuarios.component.ts
@@ -1,18 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+// Servicio de usuarios
+import { UsuariosService } from '../../services/usuarios.service';
+
+// Modelo de usuarios
+import { Usuario } from '../../modelo/usuario';
+
 @Component({
   selector: 'app-admin-usuarios',
   templateUrl: './admin-usuarios.component.html',
-  styleUrls: ['./admin-usuarios.component.scss']
+  styleUrls: ['./admin-usuarios.component.scss'],
 })
 export class AdminUsuariosComponent implements OnInit {
-
   filterUsuarios = '';
-  usuarios;
+  usuarios: Array<Usuario>;
 
-  constructor() { }
+  // Inyectar servicio de usuarios
+  constructor(private usuariosService: UsuariosService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  // Auxiliar para poblar el array de usuarios
+  getUsuarios() {
+    this.usuariosService.obtenerUsuarios.subscribe(
+      (response: any) => {
+        if (response.usuarios) this.usuarios = response.usuarios;
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
-
 }
