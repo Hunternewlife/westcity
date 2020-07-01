b989ac4e9bba9bd33e9314fff7fe965a4f25d8d1
Jonathan López <jonathan_lopez15@outlook.com>
2020-07-01 07:31:02 -0500

    componente peliculas con estilos completos incluido resposive, autollenado con la lista de peliculas y de la seccion de destacados

--- front/src/app/components/peliculas/peliculas.component.ts
+++ front/src/app/components/peliculas/peliculas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PeliculasService } from "../../services/peliculas.service";
+import { Pelicula } from "../../modelo/pelicula";
 
 @Component({
   selector: 'app-peliculas',
@@ -7,11 +9,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeliculasComponent implements OnInit {
 
+  public peliculaInfo: Pelicula;
+  public peliculas: Array<Pelicula>;
+  public ruta: String;
   public detallesPelicula = false;
+  public peliParams = {detalles: false, pelicula: new Pelicula('', '',' ', 0, '', '', '', 'activo', [], '')}
+  public filterPost = '';
 
-  constructor() { }
+
+  /* public estado = false */
+  ;
+
+  constructor(private peliculasService: PeliculasService) {
+    this.ruta = peliculasService.url;
+    this.peliculaInfo = new Pelicula('', '',' ', 0, '', '', '', 'activo', [], '');
+   }
 
   ngOnInit(): void {
+    this.listarPeliculas()
+
+    if (JSON.parse(localStorage.getItem('peliParams')) && JSON.parse(localStorage.getItem('peliParams')).detalles === true) {
+      this.detallesPelicula = JSON.parse(localStorage.getItem('peliParams')).detalles;
+      this.peliculaInfo = JSON.parse(localStorage.getItem('peliParams')).pelicula;
+    }else{
+      localStorage.setItem("peliParams", JSON.stringify(this.peliParams))
+    }
+  }
+
+  listarPeliculas(){
+    this.peliculasService.obtenerPeliculas().subscribe(
+      (response: any) => {
+        this.peliculas = response.peliculas;
+        if (!this.peliculas) {
+          alert("Lo sentimos, no hay peliculas disponibles")
+        }
+      },
+      (error) => {
+        let errorMsg = <any>error;
+        if (errorMsg != null) {
+          console.log(errorMsg)
+        }
+      }
+    );
+  }
+
+  peliIndividual(pelicula: Pelicula){
+    this.peliculaInfo = pelicula;
+    this.peliParams.detalles = true;
+    this.detallesPelicula = true;
+    this.peliParams.pelicula = pelicula;
+
+    localStorage.setItem("peliParams", JSON.stringify(this.peliParams));
+  }
+
+  listaPeliculas(){
+    this.peliParams.detalles = false;
+    this.detallesPelicula = false;
+    localStorage.setItem("peliParams", JSON.stringify(this.peliParams));
   }
 
 }
